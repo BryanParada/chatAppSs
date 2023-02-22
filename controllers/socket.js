ef1bb4a21ece44c962b50955adb06a47feb22b51
Bryan Parada <bryan.parada.cea@gmail.com>
2023-02-21 23:45:31 -0300

    FA109 - Update db when user connect

---
+++ controllers/socket.js
@@ -0,0 +1,27 @@
+const User = require('../models/user')
+
+
+const userConnected = async ( uid = '') =>{
+
+    const user = await User.findById( uid);
+    user.online = true;
+
+    await user.save(); 
+    return user;
+
+}
+
+const userDisconnect = async ( uid = '') =>{
+
+    const user = await User.findById( uid);
+    user.online = false;
+
+    await user.save(); 
+    return user;
+
+}
+
+module.exports = {
+    userConnected,
+    userDisconnect
+};
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,19 +1,11 @@
 const { checkJWT } = require('../helpers/jwt');
 const {io} = require('../index');
-const Band = require('../models/band');
-const Bands = require('../models/bands');
-
-const bands = new Bands();
-
-bands.addBand(new Band( 'Queen') );
-bands.addBand(new Band( 'Bon Jovi') );
-bands.addBand(new Band( 'Mermen') );
-bands.addBand(new Band( 'Daikaiju') );
+const { userConnected, userDisconnect} = require('../controllers/socket');
 
 // console.log(bands)
 
 //mensajes de sockets
-io.on('connection', client => { 
+io.on('connection', (client) => { 
     console.log('Client connected');
 
     //console.log( client.handshake.headers['x-token']);
@@ -21,13 +13,17 @@ io.on('connection', client => {
     const [valid, uid] = checkJWT(client.handshake.headers['x-token']);
     //console.log(valid, uid);
 
+    //VERIFICAR autenticacion
     if (!valid){return client.disconnect()};
     console.log('Client authenticated');
     
+    //Cliente autenticado
+    userConnected(uid);
     
  
     client.on('disconnect', () => {
-        console.log('Client disconnected');   
+        //console.log('Client disconnected');   
+        userDisconnect( uid );
      });
 
    //   client.on('mensaje', ( payload) =>{
