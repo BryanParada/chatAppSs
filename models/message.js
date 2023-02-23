c0edd745b064f0c054c6848cb84c96e53c152c36
Bryan Parada <bryan.parada.cea@gmail.com>
2023-02-23 17:04:26 -0300

    FA116 - Backend - Message model

---
+++ models/message.js
@@ -0,0 +1,32 @@
+
+
+const {Schema, model} = require('mongoose');
+
+const MessageSchema = Schema({
+
+    from: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    to: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }, 
+    message: {
+        type: String,
+        required: true
+    }
+    
+
+}, {//CONFIGURACION ADICIONAL AL ESQUEMA
+    timestamps: true
+});
+
+MessageSchema.method('toJSON', function(){
+    const {__v, _id, ...restOfProperties} = this.toObject();
+    return restOfProperties;
+});
+
+module.exports = model('Message', MessageSchema);
